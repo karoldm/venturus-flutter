ba13792b95f78881e86c93f106d3690a14155037
karolyne marques <karoldm1579@gmail.com>
2025-08-28 19:07:38 -0300

    feat: signup and signout testes

--- recipes/lib/ui/auth/auth_view.dart
+++ recipes/lib/ui/auth/auth_view.dart
@@ -16,7 +16,7 @@ class _AuthViewState extends State<AuthView>
   final viewModel = getIt<AuthViewModel>();
 
   late AnimationController _animationController;
-  late Animation<double> _animation;
+  // late Animation<double> _animation;
 
   @override
   void initState() {
@@ -24,38 +24,38 @@ class _AuthViewState extends State<AuthView>
 
     // ao iniciar o estado, o _animationController fornece
     // valores em intervalos de 0 a 1
-    _animationController =
-        AnimationController(
-          // vsync é um TikerProvider que ajuda a controlar a animação
-          // o Ticker envia um sinal de "tick" a cada frame
-          // e o AnimationController usa isso para atualizar a animação
-          // O tikerProvider nesse caso é fornecido pelo mixin SingleTickerProviderStateMixin
-          // Obs: SingleTickerProviderStateMixin porque eu tenho só um AnimationController
-          // se tivesse mais de um, eu usaria TickerProviderStateMixin
-          vsync: this,
-          duration: const Duration(milliseconds: 1000),
-        )..addStatusListener((listener) {
-          if (listener == AnimationStatus.completed) {
-            _animationController.reverse();
-          } else if (listener == AnimationStatus.dismissed) {
-            _animationController.forward();
-          }
-        });
+    // _animationController =
+    //     AnimationController(
+    //       // vsync é um TikerProvider que ajuda a controlar a animação
+    //       // o Ticker envia um sinal de "tick" a cada frame
+    //       // e o AnimationController usa isso para atualizar a animação
+    //       // O tikerProvider nesse caso é fornecido pelo mixin SingleTickerProviderStateMixin
+    //       // Obs: SingleTickerProviderStateMixin porque eu tenho só um AnimationController
+    //       // se tivesse mais de um, eu usaria TickerProviderStateMixin
+    //       vsync: this,
+    //       duration: const Duration(milliseconds: 1000),
+    //     )..addStatusListener((listener) {
+    //       if (listener == AnimationStatus.completed) {
+    //         _animationController.reverse();
+    //       } else if (listener == AnimationStatus.dismissed) {
+    //         _animationController.forward();
+    //       }
+    //     });
 
     // Tween define o intervalo de valores que a animação vai percorrer
     // nesse caso, de 50 a 200 (pode ser pixel, pode ser opacity, etc)
     // fazemos isso para mapear os valores gerados pelo AnimationController
     // de 0 a 1 para um intervalo de 100 a 120 (ou qualquer intervalo que você quiser)
-    _animation =
-        Tween<double>(begin: 100.0, end: 120.0).animate(_animationController)
-          ..addListener(() {
-            // quando a animação é atualizada, o setState é chamado
-            // para atualizar a UI com o novo valor da animação
-            setState(() {});
-          });
+    // _animation =
+    //     Tween<double>(begin: 100.0, end: 120.0).animate(_animationController)
+    //       ..addListener(() {
+    //         // quando a animação é atualizada, o setState é chamado
+    //         // para atualizar a UI com o novo valor da animação
+    //         setState(() {});
+    //       });
 
     // forward inicia a animação
-    _animationController.forward();
+    //_animationController.forward();
   }
 
   @override
